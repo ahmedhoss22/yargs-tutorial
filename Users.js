aa16b0ba633af132ac3356489e1cdfafbe4347d3
Hoss <101903015+ahmedhoss22@users.noreply.github.com>
2023-03-04 13:28:14 +0200

    Adding average and total

--- Users.js
+++ Users.js
@@ -1,22 +1,28 @@
 const fs=require('fs')
 class Users{
-    constructor(id,lname,fname,age,city){
+    constructor(id,lname,fname,age,city,grade){
         this.id=id
         this.lname=lname
         this.fname=fname
         this.city=city
         this.age=age
+        this.grade=grade
     }
     save(){
         let data=Users.getAll()
         let filter= data.filter((ele)=>ele.id == this.id)
         if(filter.length!=0) return console.log("Data repeated");
+        console.log(this.grade);
+        let sum =JSON.parse(this.grade).reduce((prev,curr)=> prev+=curr,0)
+        console.log(sum);
         data.push({
             id:this.id,
             fname:this.fname,
             lname:this.lname,
             age:this.age,
-            city:this.city
+            city:this.city,
+            total: sum,
+            average: sum/6,
         })
         fs.writeFileSync('data.json',JSON.stringify(data))
     }
--- index.js
+++ index.js
@@ -19,11 +19,15 @@ yargs.command({
             describe: "adding the id",
             demandOption: true,
             type: "string"
+        },
+        grade: {
+            describe: "Adding array of 6 marks",
+            demandOption: true,
         }
     },
     handler: (argv) => {
         console.log("Adding new user...");
-        let user = new Users(argv.id, argv.lname, argv.fname,argv.age,argv.city)
+        let user = new Users(argv.id, argv.lname, argv.fname,argv.age,argv.city,argv.grade)
         user.save()
     }
 })
